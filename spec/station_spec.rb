38b5a2083f6e9c0e6e9ae00d9a24274837bd9404
Jack Bittiner <jack@Jacks-MacBook-Pro.local>
2017-03-22 10:33:14 +0000

    Add test for setting zone

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,10 +1,14 @@
 require 'station'
 
 describe Station do
-  subject(:station) {described_class.new("Tooting")}
+  subject(:station) {described_class.new("Tooting", 3)}
 
 
-  it 'Checks name has been set' do
+  it 'Checks name has been set correctly' do
     expect(station.name).to eq "Tooting"
+  end
+
+  it 'Checks zone has been set correctly' do
+    expect(station.zone).to eq 3
   end
 end
